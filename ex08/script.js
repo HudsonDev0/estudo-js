41c5273ec9b260c0ee846b7d0fc7b0ff05a16f4a
HudsonDev0 <hudsonmartins00@gmail.com>
2025-06-04 21:37:17 -0300

    while

---
+++ ex08/script.js
@@ -0,0 +1,21 @@
+let c = 1
+
+/*
+while(c <= 6){
+    console.log(`Tudo bem? ${c}`)
+    c++
+}
+*/
+do{
+    console.log(`Tudo bem? ${c}`)
+    c++
+}while(c <= 6)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
